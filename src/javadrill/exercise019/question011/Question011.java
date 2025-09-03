4397381550c4002b017b071a2d0986503f111c83
nakamura-isol <eri-nakamura@i-sol.co.jp>
2025-09-03 15:51:55 +0900

    テスト

---
+++ src/javadrill/exercise019/question011/Question011.java
@@ -0,0 +1,36 @@
+package javadrill.exercise019.question011;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Question011 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("最初の整数を入力してください。");
+		try {
+			int num1 = sc.nextInt();
+
+			System.out.println("2つ目の整数を入力してください。");
+			int num2 = sc.nextInt();
+			int result = num1 / num2;
+
+			// 0で割り算をしようとした場合の例外処理
+			if (num2 == 0) {
+				throw new ArithmeticException("エラー: 0で割り算はできません。");
+			}
+
+			System.out.println(result);
+		} catch (ArithmeticException e) {
+			System.out.println("0で割ることはできません。");
+		} catch (InputMismatchException e) {
+			System.out.println("整数以外の数字が入力されました。");
+		}
+
+	}
+
+	private static int convert(String str) {
+		return Integer.parseInt(str);
+	}
+}
