f461e2cdd381a3aa889e3c37a86950c3b168f2a1
nakamura-isol <eri-nakamura@i-sol.co.jp>
2025-09-06 13:25:38 +0900

    Revert "018 オブジェクト指向の深化"
    This reverts commit 8ab860d708ab631cd26d3a707baf6c34f96c6dd5.

--- src/javadrill/exercise019/question017/RethrowExample.java
+++ src/javadrill/exercise019/question017/RethrowExample.java
@@ -3,33 +3,8 @@ package javadrill.exercise019.question017;
 public class RethrowExample {
 
 	public static void main(String[] args) {
-		try {
-			performOperation();
-		} catch (CustomException e) {
-			System.out.println("メインメソッド: " + e.getMessage());
-		}
-	}
-
-	static void performOperation() throws CustomException {
-		try {
-			// 何らかの操作を試みる
-			// 例外のテスト
-			// ゼロ除算でArithmeticExceptionが発生
-			int x = 10 / 0;
-		} catch (Exception e) {
-			String errorMessage = "performOperation メソッド内でエラーが発生しました";
-			// CustomExceptionオブジェクトの作成
-			CustomException customException = new CustomException(errorMessage, e);
-			// 新しく生成したCustomExceptionを再スロー
-			throw customException;
-		}
-	}
-}
-
-class CustomException extends Exception {
-	// コンストラクタにエラーメッセージと元の例外を渡す
-	CustomException(String message, Throwable cause) {
-		super(message, cause);
+		// TODO 自動生成されたメソッド・スタブ
 
 	}
+
 }
--- src/javadrill/exercise019/question027/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package javadrill.exercise019.question027;
-
-public class InsufficientFundsException extends Exception{
-	InsufficientFundsException(String message){
-		super(message);
-	}
-}
